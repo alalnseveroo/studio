106fe91d1ca2b7b5c1c4ad4cacec64a3950321db
Allan Severo <allan.ruptura@gmail.com>
2025-08-07 02:54:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: 'DASH: Hub de Segurança de Acesso Digital',
-  description: 'Um Hub de Segurança de Acesso Digital com autenticação Supabase.',
+  description': 'Um Hub de Segurança de Acesso Digital com autenticação Supabase.',
 };
 
 export default function RootLayout({
@@ -18,7 +18,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
       </head>
       <body className="font-body antialiased">
         {children}
