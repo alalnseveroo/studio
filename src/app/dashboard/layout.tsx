944a3357e515873ae33b6bb54ec5c1adb75b2801
Allan Severo <allan.ruptura@gmail.com>
2025-08-07 00:56:06 +0000

    Em vermelho deveria ser branco apenas ali

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,6 +7,7 @@ import {
   Menu,
   Package,
   Package2,
+  Search,
   ShoppingCart,
   Users,
 } from 'lucide-react'
@@ -30,6 +31,7 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { SidebarProvider, Sidebar, SidebarTrigger, SidebarContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarFooter, SidebarHeader, SidebarSeparator, SidebarInset } from '@/components/ui/sidebar'
 
 export default function DashboardLayout({
   children,
@@ -38,7 +40,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+      <div className="hidden border-r bg-sidebar md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
@@ -208,7 +210,7 @@ export default function DashboardLayout({
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex flex-1 flex-col bg-background">
           {children}
         </main>
       </div>
