15a01afd5615fbe24451f7b6b0a2adaab5367b6d
Allan Severo <allan.ruptura@gmail.com>
2025-08-07 11:45:12 +0000

    A ideia é que seja branco a parte principal e os cards brancos também. E

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -41,7 +41,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+      <div className="hidden border-r bg-card md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
@@ -94,7 +94,7 @@ export default function DashboardLayout({
             </SheetContent>
           </Sheet>
         </header>
-        <main className="flex flex-1 flex-col bg-background">
+        <main className="flex flex-1 flex-col bg-card">
           {children}
         </main>
       </div>
