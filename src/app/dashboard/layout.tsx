5435e66b228471fe537d224d2a94b5bab647d3cd
Allan Severo <allan.ruptura@gmail.com>
2025-08-13 01:30:46 +0000

    Não resolveu.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -134,15 +134,15 @@ function DashboardHeader() {
 
      return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="container flex h-16 items-center">
-                <div className="flex items-center">
-                    <Link href="/dashboard" className="mr-6 flex items-center space-x-2">
+            <div className="container flex h-16 items-center justify-between">
+                <div className="flex items-center gap-6">
+                    <Link href="/dashboard" className="flex items-center space-x-2">
                         <Package2 className="h-6 w-6" />
                         <span className="hidden font-bold sm:inline-block">Virtei</span>
                     </Link>
                     <MainNav />
                 </div>
-                <div className="ml-auto flex items-center gap-4">
+                <div className="flex items-center gap-4">
                     <Button variant="ghost" size="icon">
                         <Bell className="h-5 w-5" />
                         <span className="sr-only">Notificações</span>
