5d1de49dcb14df13cf94cf2de4498c682f756a5d
Allan Severo <allan.ruptura@gmail.com>
2025-08-16 22:00:13 +0000

    Me entregue um sql para colocar o token api da assas criptofrado para o

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,7 @@ const nextConfig: NextConfig = {
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvdXlubXJibHp2d2xocmZ5aW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwMDk3ODMsImV4cCI6MjA3MDU4NTc4M30.ShP6C-DXUAUo18g1eoG6c5V_y6Dv7fvJQ3bn8S-9EbM',
     BREVO_API_KEY: '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',
     NEXT_PUBLIC_SITE_URL: process.env.NODE_ENV === 'production' ? 'https://your-production-url.com' : 'http://localhost:9002',
+    // As chaves do Asaas serão carregadas do arquivo .env e não precisam ser listadas aqui
   }
 };
 
