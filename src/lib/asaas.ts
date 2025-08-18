ec2b731600d463a78563475d7f4d0d0c8f518cb3
Allan Severo <allan.ruptura@gmail.com>
2025-08-18 16:49:22 +0000

    Altere para essa api (essa é de produção): $aact_prod_000MzkwODA2MWY2OGM

--- src/lib/asaas.ts
+++ src/lib/asaas.ts
@@ -3,9 +3,9 @@
 
 import type { Profile } from "./types";
 
-const ASAAS_API_URL = 'https://api-sandbox.asaas.com/v3';
+const ASAAS_API_URL = 'https://api.asaas.com/v3';
 // A chave de API está definida diretamente para garantir o funcionamento.
-const ASAAS_API_KEY = '$aact_hmlg_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6Ojg3MzU5ODExLWJlYjEtNGMyMC1iNTgyLWFkOWI1YzQ5OWIzYTo6JGFhY2hfZjE3NDA1NDMtY2M2My00ZTc3LTg3NzktZTIwNDBiZjhjY2Jh';
+const ASAAS_API_KEY = '$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6Ojk0OTI5YTA4LWMwNGEtNDUwMy04YmU0LWZhZWU2MWQyOTAwNDo6JGFhY2hfMjgxN2I2NTktYmIyNi00Y2Y3LWExMmItM2RjODNiODUxODg2';
 
 type AsaasCustomer = {
     id: string;
