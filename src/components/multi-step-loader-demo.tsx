f51ce8b23d60f0a78cdbcef3988b0c1692b2c1f2
Allan Severo <allan.ruptura@gmail.com>
2025-08-22 16:14:44 +0000

    Perfeito. Mas ao invés de aparecer na tela o MultiStepLoader, enquanto c

--- src/components/multi-step-loader-demo.tsx
+++ src/components/multi-step-loader-demo.tsx
@@ -31,32 +31,11 @@ const loadingStates = [
 ];
 
 export default function MultiStepLoaderDemo() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   return (
     <div className="w-full h-screen flex items-end justify-end p-4">
       {/* Core Loader Modal */}
       <Loader loadingStates={loadingStates} loading={loading} duration={2000} />
-
-      {/* The buttons are for demo only, remove it in your actual code ⬇️ */}
-      <button
-        onClick={() => setLoading(true)}
-        className="bg-[#39C3EF] hover:bg-[#39C3EF]/90 text-black mx-auto text-sm md:text-base transition font-medium duration-200 h-10 rounded-lg px-8 flex items-center justify-center"
-        style={{
-          boxShadow:
-            "0px -1px 0px 0px #ffffff40 inset, 0px 1px 0px 0px #ffffff40 inset",
-        }}
-      >
-        Click to load
-      </button>
-
-      {loading && (
-        <button
-          className="fixed top-4 right-4 text-black dark:text-white z-[120]"
-          onClick={() => setLoading(false)}
-        >
-          <XSquare className="h-10 w-10" />
-        </button>
-      )}
     </div>
   );
 }
