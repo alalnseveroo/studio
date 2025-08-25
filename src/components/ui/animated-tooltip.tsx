237a698fb18cc39bd7ddf43d5125365294880910
Allan Severo <allan.ruptura@gmail.com>
2025-08-25 15:59:17 +0000

    Error: Encountered two children with the same key, `Allan Rodrigues`. Ke

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -44,7 +44,7 @@ export const AnimatedTooltip = ({
       {items.map((item, idx) => (
         <div
           className="group relative -mr-4"
-          key={item.name}
+          key={item.id}
           onMouseEnter={() => setHoveredIndex(item.id)}
           onMouseLeave={() => setHoveredIndex(null)}
         >
@@ -92,5 +92,3 @@ export const AnimatedTooltip = ({
     </>
   );
 };
-
-    
