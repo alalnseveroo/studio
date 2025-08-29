61994ca17b0eabfcb8311a3359bb5627d87f02f6
Allan Severo <allan.ruptura@gmail.com>
2025-08-29 16:11:56 +0000

    Error: In HTML, <div> cannot be a descendant of <p>. This will cause a h

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -156,7 +156,7 @@ function TaskList({ tasks, clients, onTaskUpdate, onTaskCreate }: { tasks: Task[
         <div className="flex flex-col h-full">
             <CardHeader>
                 <CardTitle className="text-lg font-semibold">Lista de Tarefas</CardTitle>
-                <CardDescription>
+                <CardDescription as="div">
                      <div className="relative">
                         <Input 
                             {...register("description")} 
